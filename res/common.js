05958147720c2d9a43e0ce0f2a7e1ab0c221cbbb
Re Frain (extends 黐麟) <35806750+re-frain-org@users.noreply.github.com>
2019-11-10 01:47:13 +0900

    Add files via upload

--- res/common.js
+++ res/common.js
@@ -1,4 +1,9 @@
 "use strict";
+/**
+ * @function Element#prependChild
+ * @argument Element element
+ * @this Element
+ */
 Element.prototype.prependChild = function(element) {
 	this.insertBefore(element, this.firstChild);
 };
@@ -28,16 +33,18 @@ function createListItem({category = "other", image = "", title = "", url = ""}) 
 	W1JhWxuv.appendChild(TVOxHpZ9);
 	c6JO6k62.prependChild(W1JhWxuv);
 }
-firebase.initializeApp({
-	apiKey: "AIzaSyCAtEoI6peuGpSvRlvTkjspSU0LxIX4cb4",
-	appId: "1:192927222320:web:7a6c7b159ce8495d",
-	authDomain: "re-frain-org.firebaseapp.com",
-	databaseURL: "https://re-frain-org.firebaseio.com",
-	messagingSenderId: "192927222320",
-	projectId: "re-frain-org",
-	storageBucket: "re-frain-org.appspot.com"
-});
-const database = firebase.database();
+/**
+ * @function parseQuery
+ * @return {string: string}
+ */
+function parseQuery() {
+	const parameter = {};
+	location.search.slice(1).split("&").forEach(query => {
+		const [key, value = ""] = query.split("=");
+		parameter[key] = value;
+	});
+	return parameter;
+}
 mWYXHFlu.addEventListener("keydown", event => {
 	switch (event.keyCode) {
 		case 13:
@@ -49,3 +56,13 @@ mWYXHFlu.addEventListener("keydown", event => {
 	}
 });
 SYaLdKxJ.addEventListener("click", event => location.href = `/search/?keyword=${mWYXHFlu.value}`);
+firebase.initializeApp({
+	apiKey: "AIzaSyCAtEoI6peuGpSvRlvTkjspSU0LxIX4cb4",
+	appId: "1:192927222320:web:7a6c7b159ce8495d",
+	authDomain: "re-frain-org.firebaseapp.com",
+	databaseURL: "https://re-frain-org.firebaseio.com",
+	messagingSenderId: "192927222320",
+	projectId: "re-frain-org",
+	storageBucket: "re-frain-org.appspot.com"
+});
+const database = firebase.database();
--- search/script.js
+++ search/script.js
@@ -1,13 +1,16 @@
 "use strict";
-const search = location.search.slice(1);
-const parameter = {};
-search.split("&").forEach(query => {
-	const [key, value] = query.split("=");
-	parameter[key] = value;
-});
-if (parameter.keyword !== undefined && parameter.keyword !== "") {
+const {keyword} = parseQuery();
+if (keyword !== undefined && keyword !== "") {
+	const a = document.createElement("a");
+	a.href = `/search/?keyword=${keyword}`;
+	a.textContent = keyword;
+	a.setAttribute("class", "TnBfB6P0");
+	const li = document.createElement("li");
+	li.setAttribute("class", "tvSFfqLi");
+	li.appendChild(a);
+	YFoSPTA3.appendChild(li);
 	database.ref("news").once("value", snapshot => {
 		const val = snapshot.val();
-		if (val !== null) Object.values(val).filter(value => value.title.toString().match(new RegExp(parameter.keyword))).forEach(createListItem);
+		if (val !== null) Object.values(val).filter((value = "") => String(value.title).match(new RegExp(keyword))).forEach(createListItem);
 	});
 }
